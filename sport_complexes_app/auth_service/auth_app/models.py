48a17e642adea109a8177d410bdeca3be0663994
KirillRyrm <rirmakkirill@gmail.com>
2025-04-11 13:04:44 +0300

    Added the database connecttion and model UserCredentials

--- sport_complexes_app/auth_service/auth_app/models.py
+++ sport_complexes_app/auth_service/auth_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import Group
 
 # Create your models here.
+class UserCredentials(models.Model):
+    user_credential_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=64, unique=True)
+    user_role = models.CharField(max_length=32, default='client')
+    password = models.CharField(max_length=255)
+    groups = models.ManyToManyField(Group, related_name='user_credentials', blank=True)
+
+    class Meta:
+        managed = False
+        db_table = 'user_credentials'
--- sport_complexes_app/auth_service/auth_service/settings.py
+++ sport_complexes_app/auth_service/auth_service/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'auth_app'
 ]
 
 MIDDLEWARE = [
@@ -73,10 +75,21 @@ WSGI_APPLICATION = 'auth_service.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sport_complexes',
+        'USER': 'postgres',
+        'PASSWORD': 'rirmakkirill890',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
